02326075a4cde0d1a553a045eeaf502574695513
royesh <muhammadhussian.royesh1@gmail.com>
2025-09-02 16:36:58 +0200

    early afternoon update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,25 +12,25 @@ const Navbar = () => {
   return (
     <div className="navbar">
         <div className='logo'>
-            <img src={logo} alt="sawghat logo" />
-            <h5>Sawghat Onine Store</h5>
+            <Link to="/">    <img src={logo} alt="sawghat logo" /> </Link>
+            <h5> <Link to="/" style={{textDecoration:'none'}}> Sawghat Online Store</Link> </h5>
         </div>
         <div className='nav-items'>
             <ul className='nav-menu'>
-                <li onClick={()=>{setHrmenue("shop")}}> <Link to="/">Shop</Link> {hrmeune ==="shop" ?<hr/> :<></> }  </li>
+                <li onClick={()=>{setHrmenue("shop")}}> <Link to="/" style={{textDecoration:'none'}}>Shop</Link> {hrmeune ==="shop" ?<hr/> :<></> }  </li>
                
-                <li onClick={()=>{setHrmenue("men")}}> <Link to="/men">Men</Link> {hrmeune ==="men" ?<hr/> :<></>}</li>
-                <li onClick={()=>{setHrmenue('women')}}> <Link to="Women">Women</Link> {hrmeune ==="women" ?<hr/> :<></>} </li>
-                <li onClick={()=>{setHrmenue("kids")}}> <Link to="kids">Kids</Link> {hrmeune ==="kids"?<hr/>:<></>} </li>
+                <li onClick={()=>{setHrmenue("men")}}> <Link to="/men" style={{textDecoration:'none'}}>Men</Link> {hrmeune ==="men" ?<hr/> :<></>}</li>
+                <li onClick={()=>{setHrmenue('women')}}> <Link to="Women" style={{textDecoration:'none'}}>Women</Link> {hrmeune ==="women" ?<hr/> :<></>} </li>
+                <li onClick={()=>{setHrmenue("kids")}}> <Link to="kids" style={{textDecoration:'none'}}>Kids</Link> {hrmeune ==="kids"?<hr/>:<></>} </li>
             </ul>
         </div>
         <div className='nav-right'>
         <div className="login">
-            <a href="/login"> <Link to="/login">Login</Link></a>
+            <a href="/login"> <Link to="/login" style={{textDecoration:"none"}}>Login</Link></a>
         </div>
         <div className='cart'>
 
-           <Link to="cart"> <img src={cart_logo} alt="Cart-Logo" /> </Link>
+           <Link to="cart" style={{textDecoration:'none'}}> <img src={cart_logo} alt="Cart-Logo" /> </Link>
             <div className='cart-count'> 0</div>
         </div>
         </div>
