9427a36737bccc5f9cb4973a4e0ab7f465b06cbe
royesh <muhammadhussian.royesh1@gmail.com>
2025-09-10 12:27:29 +0200

    a little bit change

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,15 @@ import Footer from './Components/Footer/Footer'
 import man_banner from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
 import kids_banner from './Components/Assets/banner_kids.png'
+ 
 
 function App() {
   return (
     <>
+
       <BrowserRouter>
       <Navbar/>
+              
       <Routes>
         <Route path='/' element={<Shop />} />
         <Route path="/men" element={<ShopCategory banner={man_banner} category="men" />}>
